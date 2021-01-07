cde8e3515d566ff9ecf509f47f9bab7a4873bda1
JongHyuck <fkaus113@gmail.com>
2021-01-07 23:07:45 +0900

    프로토 타입의 필요성을 재현하기 위한 무식한 코드

---
+++ PrototypePattern/PrototypePattern.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Monster 
+{
+public:
+	virtual void Bark() = 0;
+};
+
+class Ghost : public Monster 
+{
+public:
+	virtual void Bark() {
+		cout << "Ghoooooooooost" << endl;
+	}
+};
+class Slime : public Monster 
+{
+public:
+	virtual void Bark() {
+		cout << "slim slim slimmeeee" << endl;
+	}
+};
+class Demon : public Monster 
+{
+public:
+	virtual void Bark() {
+		cout << "DEMON!@!!!!" << endl;
+	}
+};
+
+class Spawner 
+{
+public:
+	virtual ~Spawner() {}
+	virtual Monster* SpawnMonster() = 0;
+};
+
+class GhostSpawner : public Spawner
+{
+public:
+	virtual Monster* SpawnMonster() {
+		return new Ghost();
+	}
+};
+
+class SlimeSpawner : public Spawner
+{
+public:
+	virtual Monster* SpawnMonster() {
+		return new Slime();
+	}
+};
+
+class DemonSpawner : public Spawner
+{
+public:
+	virtual Monster* SpawnMonster() {
+		return new Demon();
+	}
+};
+
+int main()
+{
+	GhostSpawner* gs = new GhostSpawner();
+	Ghost* ghost = reinterpret_cast<Ghost*>(gs->SpawnMonster());
+	ghost->Bark();
+	return 0;
+}
