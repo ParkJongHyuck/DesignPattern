48712a0ace2b7d5b484ef6b0b36cb2d62af6c2cc
JongHyuck <fkaus113@gmail.com>
2021-03-03 23:02:06 +0900

    상태 패턴의 필요성. 1. 캐릭터 상태에 따른 분기가 많이생긴다.   (점프 중일때, 엎드려있을때, 걸을때 ... ) 2. 각 행동이 불가능한 상태인지 알기 위해서 많은 플래그값이 필요하다.   (점프 불가능한 상태 : 점프중, 맞는 중, 넘어지는 중 ... )

---
+++ StatePattern/StatePattern.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+using namespace std;
+
+enum class Input {PRESS_DOWN, PRESS_UP, RELEASE_DOWN};
+
+class Hero
+{
+public:
+	void HandleInput(Input input)
+	{
+		if (input == Input::PRESS_UP)
+		{
+			if (!isJumping) 
+			{
+				isJumping = true;
+				// 점프
+			}
+		}
+		else if (input == Input::PRESS_DOWN)
+		{
+			if (!isJumping)
+			{
+				// 엎드리기
+			}
+		}
+		else if (input == Input::RELEASE_DOWN)
+		{
+			// 기본상태
+		}
+	}
+
+private:
+	bool isJumping = false;
+};
+
+int main()
+{
+
+	return 0;
+}
