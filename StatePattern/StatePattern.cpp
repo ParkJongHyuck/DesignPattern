5ddb7298d3d912b79e0aa995ba13512ac15b32be
JongHyuck <fkaus113@gmail.com>
2021-03-04 00:15:31 +0900

    캐릭터가 들고 있는 무기에 따라서 동작이 달라진다면 병행 상태 기계를 사용해보자. 상태 기계를 2개 둠으로써 문제가 해결된다. 두 상태가 서로에게 영향을 끼치지 않을 경우 유용하다.

--- StatePattern/StatePattern.cpp
+++ StatePattern/StatePattern.cpp
@@ -8,11 +8,9 @@ enum class State { STATE_STANDING, STATE_JUMPING, STATE_DUCKING };
 class HeroState {
 public:
 	virtual ~HeroState() {}
-	// [1] 상태에 의존하는 모든 코드를 가상함수로 만든다.
 	virtual void HandleInput(Hero& hero, Input input) {}
 	virtual void Update(Hero& hero) {}
 
-	// [4] 각 상태들을 정적 인스턴스로 둔다.
 	static DuckingState ducking;
 	static JumpingState jumping;
 	static StandingState standing;
@@ -22,21 +20,27 @@ public:
 class Hero
 {
 public:
-	// [3] 상태 객체에 동작을 위임한다.
 	void HandleInput(Input input)
 	{
 		state->HandleInput(*this, input);
+		equipment->HandleInput(*this, input);
 	}
 
 	void Update() 
 	{
 		state->Update(*this);
+		equipment->Update(*this);
 	}
 
+	// 캐릭터가 만약 무기를 든다고 하면 어떻게 해야할까.
+	// 비무장 상태 m개, 무장 상태 m개 ... 매우 많은 상태의 정의가 필요하다.
+	// 그러나 이를 상태 기계를 2개를 둔다면 해결할 수 있다.
+	// 아래와 같이 하나의 상태 기계는 '무엇을 하는가'를 다루고 다른 하나는 '무엇을 들고 있는가'를 다룬다.
+	// 따라서 상태는 m + n 개만 있으면 된다.
 	HeroState* state;
+	HeroState* equipment;
 };
 
-// [2] case 별로 있었던 상태들을 클래스로 정의한다.
 class DuckingState : public HeroState {
 public:
 	DuckingState() {}
