552941d981c9f818c76fb4b7787764b4e843e69e
JongHyuck <fkaus113@gmail.com>
2021-03-07 02:20:01 +0900

    박스를 받았음을 current와 Next로 나누어 읽기용, 쓰기용으로 버퍼를 두개 둔다. 이러면 순서가 바뀌어도 똑같은 행동을 할 수 있게 된다.

--- DoubleBufferPattern/DoubleBufferPattern.cpp
+++ DoubleBufferPattern/DoubleBufferPattern.cpp
@@ -5,7 +5,7 @@ using namespace std;
 class DeliveryMan
 {
 public:
-	DeliveryMan() : hasBox(false) {}
+	DeliveryMan() : currentBox(false), nextBox(false) {}
 	
 	void Update() {
 		if (HasBox()) {
@@ -14,14 +14,19 @@ public:
 		}
 	}
 
+	void Swap() {	// Reset대신 Swap을 사용
+		currentBox = nextBox;
+		nextBox = false;
+	}
+
 	void SetNextDeliveryMan(DeliveryMan* next_) { next = next_; }
-	void Reset() { hasBox = false; }
-	void SendBox() { hasBox = true; }
-	bool HasBox() { return hasBox; }
+	void SendBox() { nextBox = true; }
+	bool HasBox() { return currentBox; }
 
 private:
 	DeliveryMan* next = nullptr;
-	bool hasBox;
+	bool currentBox;	// 읽기 용도
+	bool nextBox;		// 쓰기 용도
 };
 
 class Stage
@@ -34,8 +39,11 @@ public:
 	void Update() {
 		for (int i = 0; i < NUM_MAN; ++i) {
 			deliveryMan[i]->Update();
-			deliveryMan[i]->Reset();
 		}
+		for (int i = 0; i < NUM_MAN; ++i) {
+			deliveryMan[i]->Swap();
+		}
+		// 먼저 모든 배우를 업데이트 한 후 상태를 교체
 	}
 private:
 	static const int NUM_MAN = 3;
