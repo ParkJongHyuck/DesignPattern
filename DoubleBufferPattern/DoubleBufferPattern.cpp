0836bd8e45a4d59afad352648c511703008c8d40
JongHyuck <fkaus113@gmail.com>
2021-03-06 23:41:29 +0900

    이중 버퍼 패턴의 필요성. tom -> dave -> cat -> tom 순으로 이어져있다. tom이 물건을 먼저 보내고 update를 시작하면 tom, dave, cat 모두 물건을 보내게 되지만, cat이 물건을 먼저 보내게되면 반복문 실행 순어 때문에 tom과 dave는 다음 update에 보내게 된다.
    이러한 순서의 의존성을 없애기위해서 이중 버퍼 패턴이 필요하다

---
+++ DoubleBufferPattern/DoubleBufferPattern.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class DeliveryMan
+{
+public:
+	DeliveryMan() : hasBox(false) {}
+	
+	void Update() {
+		if (HasBox()) {
+			cout << "carry out" << endl;
+			next->SendBox();
+		}
+	}
+
+	void SetNextDeliveryMan(DeliveryMan* next_) { next = next_; }
+	void Reset() { hasBox = false; }
+	void SendBox() { hasBox = true; }
+	bool HasBox() { return hasBox; }
+
+private:
+	DeliveryMan* next = nullptr;
+	bool hasBox;
+};
+
+class Stage
+{
+public:
+	void Add(DeliveryMan* man, int index) {
+		deliveryMan[index] = man;
+	}
+
+	void Update() {
+		for (int i = 0; i < NUM_MAN; ++i) {
+			deliveryMan[i]->Update();
+			deliveryMan[i]->Reset();
+		}
+	}
+private:
+	static const int NUM_MAN = 3;
+	DeliveryMan* deliveryMan[NUM_MAN];
+};
+int main()
+{
+	Stage* stage = new Stage();
+	DeliveryMan* tom = new DeliveryMan();
+	DeliveryMan* dave = new DeliveryMan();
+	DeliveryMan* cat = new DeliveryMan();
+
+	tom->SetNextDeliveryMan(dave);
+	dave->SetNextDeliveryMan(cat);
+	cat->SetNextDeliveryMan(tom);
+
+	stage->Add(tom, 0);
+	stage->Add(dave, 1);
+	stage->Add(cat, 2);
+
+	cat->SendBox();
+
+	for (int i = 0; i < 3; ++i)
+	{
+		cout << "[" << i << "]" << endl;
+		stage->Update();
+	}
+	
+	return 0;
+}
