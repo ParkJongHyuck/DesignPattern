679cb0bee23cc7aa04dcb06f94517e183316ed7b
JongHyuck <fkaus113@gmail.com>
2021-03-07 16:44:34 +0900

    고정 시간 간격, FixedUpdate

--- GameLoopPattern/GameLoopPattern.cpp
+++ GameLoopPattern/GameLoopPattern.cpp
@@ -14,24 +14,32 @@ void Render() {
 
 }
 
-void Update(double elapsed) {
+void Update() {
 
 }
+constexpr double MS_PER_UPDATE = 0.016;
 
 int main()
 {
-	// [1] 가변 시간 간격
-	// 이전 프레임과의 시간 차이만큼 Update를 시킴으로써
-	// fps 상관없이 동일한 계산(물리, AI)이 가능하다.
-	// 그러나 고사양의 PC에서는 fps 가 높을 것이고,
-	// Update 빈도가 높아지면 부동소수점 반올림 오류가 누적될 가능성이 있다.
+	// [2] 고정 시간 간격
+	// 가변 시간 간격에서는 PC 성능에 따라서 물리 계산의 빈도가 달라지고
+	// 이에 따라 부동 소수점 반올림 오류가 축적될 수 있다.
+	// 그래서 AI, 물리 계산을 담당하는 Update 부분을 고정된 간격으로 업데이트 한다.
+	// 이로 인해서 여러 하드웨어에서 일정한 속도로 시뮬레이션이 가능해진다.
+	// 유니티의 FixedUpdate와 동일하다.
+
 	double previous = GetCurrentTime();
+	double lag = 0.0;
 	while (true) {
 		double current = GetCurrentTime();
 		double elapsed = current - previous;
 		previous = current;
+		lag += elapsed;
 		ProcessInput();
-		Update(elapsed);
+		while (lag >= MS_PER_UPDATE) {
+			Update();
+			lag -= MS_PER_UPDATE;
+		}
 		Render();
 	}
 	return 0;
