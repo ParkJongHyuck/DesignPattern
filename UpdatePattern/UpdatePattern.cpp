0333dd16407a00262737b16691ff1fd316addbd5
JongHyuck <fkaus113@gmail.com>
2021-03-07 17:16:37 +0900

    업데이트 패턴. 각 객체들을 캡슐화하고 Update 추상 함수를 제공한다. 객체들은 순차적으로 업데이트 되고 유지보수하기 쉬워진다.

---
+++ UpdatePattern/UpdatePattern.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Entity
+{
+public:
+	virtual void Update() = 0;
+};
+
+class Slime : public Entity
+{
+public:
+	virtual void Update() {
+
+	}
+};
+
+class Player : public Entity
+{
+public:
+	virtual void Update() {
+
+	}
+};
+int main()
+{
+	vector<Entity*> gameEntity;
+
+	while (true)	// 게임 루프
+	{
+		// 유저 입력 처리
+
+		for (Entity* entity : gameEntity)
+		{
+			entity->Update();
+		}
+
+		// 화면 랜더링
+	}
+
+	return 0;
+}
